6162f9392dc118aa7a124a5b0ec3e33a1ad7697b
Bernardo <bbdmarcano83@gmail.com>
2026-05-30 07:54:51 -0400

    Ajustes de despliegue para Render

--- servidor.py
+++ servidor.py
@@ -297,5 +297,8 @@ async def obtener_historico(simbolo: str):
         return datos.get('cur_hist_mov_emisora', [])
     
 if __name__ == "__main__":
-        import uvicorn
-        uvicorn.run(app, host="127.0.0.1", port=8000)
+    import uvicorn
+    import os
+    # Obtenemos el puerto que Render nos asigna, o usamos 8000 si estamos en local
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
