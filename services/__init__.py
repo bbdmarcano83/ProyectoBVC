388d16400855a84b2ffef6fb6fa7576ff8345975
bbdmarcano83 <bbdmarcano83@gmail.com>
2026-08-28 18:54:37 -0400

    security: install fail-closed secret guard

--- services/__init__.py
+++ services/__init__.py
@@ -1,5 +1,7 @@
 """ProyectoBVC service package bootstrap."""
 
 from services.security_runtime import install_fastapi_security_bootstrap
+from services.secret_guard import install_secret_guard
 
 install_fastapi_security_bootstrap()
+install_secret_guard()
